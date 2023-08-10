90024d007c916206c31310dd4edc9c1ad2ab20b3
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-08-10 22:50:56 +0530

    Done

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// Challenge
+const os = require("os");
 
-// 1. Create a folder named "Utkarsh"
-// 2. Create a file in it name "file.txt" and add data into it
-// 3. Add more data into the file at the end of the existing data.
-// 4. Read the data without getting the buffer data at first.
-// 5. Rename the file
-// 6. Now delete both the file and the folder.
-
-const fs = require("fs");
-
-// fs.mkdir("./Utkarsh", err => console.log(`${err ? err : "Folder Created Successfully"}`));
+// console.log(os.arch())
+console.log(os.hostname())
+console.log(os.platform())
+console.log(os.type())
 
 
-// fs.writeFile("./Utkarsh/file.txt", "This is Utkarsh Singh Chouhan", err => console.log(`${err ? err : "File created Successfully"}`));
+// const freeMemory = os.freemem();
+// console.log(freeMemory)
+// console.log(`${freeMemory/1024/1024/1024}`)
 
-
-// fs.appendFile("./Utkarsh/file.txt", "THis is the data to be appended", err => console.log(`${err ? err : "Data appended successfully"}`));
-
-
-
-// fs.readFile("./Utkarsh/file.txt", (err, data) => console.log(`${err ? err : data}`));
-
-
-// fs.rename("./Utkarsh/file.txt", "./Utkarsh/abc.txt", err => console.log(`${err ? err : "file renamed successfully"}`));
-
-
-// fs.unlink("./Utkarsh/abc.txt", err => console.log(`${err ? err : "file deleted successfully"}`));
-
-
-// fs.rmdir("./Utkarsh", (err) => console.log(`${err ? err : "Folder deleted successfully"}`))
-
-
+// const totalMemory = os.totalmem();
+// console.log(totalMemory);
+// console.log(`${totalMemory/1024/1024/1024}`)
 
