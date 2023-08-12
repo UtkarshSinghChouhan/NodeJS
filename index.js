cc576e52306e3f029a2079b1fffaa7727d2b5a0c
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-08-12 19:50:15 +0530

    Done

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const path = require("path");
+const {add, sub, name} = require("./operator")
 
-
-console.log(path.dirname("./index.js"))
-console.log(path.basename("./index.js"))
-console.log(path.extname("./index.js"))
-
-
-console.log(path.parse("./index.js"))
-
-const myPath = path.parse("./index.js")
-console.log(myPath.name)
-console.log(myPath.root)
-console.log(myPath.dir)
-console.log(myPath.base)
-console.log(myPath.ext)
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(name)
---
+++ operator.js
@@ -0,0 +1,10 @@
+const add = (val1, val2) => {
+    return val1 + val2;
+}
+const sub = (val1, val2) => {
+    return val1 - val2;
+}
+
+const name = "utkarsh";
+
+module.exports = {add, sub, name};
