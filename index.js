418fb91b7b31f5c1964941b535f254a56367325f
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-08-28 08:28:20 +0530

    This way of reading the data via the stream object over the server

--- index.js
+++ index.js
@@ -16,27 +16,31 @@ server.on("request", (req, res) => {
 
     // THIS IS THE OLD WAY OF READING THE DATA VIA THE SERVER
 
-    // var fs = require("fs");
-    // fs.readFile("./input.txt", (err, data) => err ? console.log(err) : res.end(data));
+        // var fs = require("fs");
+        // fs.readFile("./input.txt", (err, data) => err ? console.log(err) : res.end(data));
 
 
 
     // THIS IS THE NEW WAY OF READING THE DATA VIA THE SERVER THROUGH STREAMING
 
 
-    const rstr = fs.createReadStream("./input.txt")  // First create a readeable stream
-    rstr.on("data", (chunkData) => {
-        res.write(chunkData)
-    })
+        // const rstr = fs.createReadStream("./input.txt")  // First create a readeable stream
+        // rstr.on("data", (chunkData) => {
+        //     res.write(chunkData)
+        // })
 
-    rstr.on("end", () => {
-        res.end();
-    })
+        // rstr.on("end", () => {
+        //     res.end();
+        // })
 
-    rstr.on("error", (err) => {
-        console.log(err);
-        res.end("File not found");
-    })
+        // rstr.on("error", (err) => {
+        //     console.log(err);
+        //     res.end("File not found");
+        // })
+
+    // THIRD WAY 
+        const rstr = fs.createReadStream("./input.txt")
+        rstr.pipe(res);
 })
 
 server.listen(8000, "127.0.0.1")
